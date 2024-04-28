db1f21a6c1934c7f5f9e9d4647f1df5081745e50
aeliilina <karybaevaaelina@gmail.com>
2024-04-28 23:10:05 +0600

    maket2

--- js/app.js
+++ js/app.js
@@ -27,18 +27,90 @@ navbarContent.forEach(function (item) {
     navbar.append(linkEl) }
 )
 
-var tableObj={
-    Quantity:['5-10', '5-10' , '5-10' , '5-10', '5-10' , '5-10' , '5-10', '5-10', '5-10'],
-    Price:['2000 сом' , '2000 сом' , '2000 сом', '2000 сом', '2000 сом' ,'2000 сом' ,'2000 сом', '2000 сом' ,'2000 сом']
+// var tableObj={
+    // Quantity:['5-10', '5-10' , '5-10' , '5-10', '5-10' , '5-10' , '5-10', '5-10', '5-10'],
+    // Price:['2000 сом' , '2000 сом' , '2000 сом', '2000 сом', '2000 сом' ,'2000 сом' ,'2000 сом', '2000 сом' ,'2000 сом']
+// }
+// var tableQuantity = document.querySelector(".tableQuantity")
+// var tablePrice = document.querySelector(".tablePrice")
+
+
+// function tableMaker(arr,tableRow){
+//     arr.forEach(function(item){
+//     var tCell = document.createElement("td")
+//     tCell.textContent= item
+//     tableRow.append(tCell)
+// })
+// }
+// for (var key in tableObj){
+//     tableMaker(tableObj[key], key==='quality'? tableQuantity:tablePrice)
+// }
+
+
+var tableObj = {
+    table1:{
+        Quantity:['5-10', '5-10' , '5-10' , '5-10', '5-10' , '5-10' , '5-10', '5-10', '5-10'],
+        Price:['2000 сом' , '2000 сом' , '2000 сом', '2000 сом', '2000 сом' ,'2000 сом' ,'2000 сом', '2000 сом' ,'2000 сом']
+    },
+    table2:{
+        Quantity:['5-10', '5-10' , '5-10' , '5-10', '5-10' , '5-10' , '5-10', '5-10', '5-10'],
+        Price:['2000 сом' , '2000 сом' , '2000 сом', '2000 сом', '2000 сом' ,'2000 сом' ,'2000 сом', '2000 сом' ,'2000 сом']
+    },
+    table3:{
+        Quantity:['5-10', '5-10' , '5-10' , '5-10', '5-10' , '5-10' , '5-10', '5-10', '5-10'],
+        Price:['2000 сом' , '2000 сом' , '2000 сом', '2000 сом', '2000 сом' ,'2000 сом' ,'2000 сом', '2000 сом' ,'2000 сом']
+    }
 }
-var tableQuantity = document.querySelector(".tableQuantity")
-var tablePrice = document.querySelector(".tablePrice")
 
-tableObj.Quantity.forEach(function(item){
-    var tCell = document.createElement("td")
-    tCell.textContent= item
+var tableWrapper = document.querySelector('.tableWrapper')
+
+function tableMaker(tableData){
+  var table = document.createElement('table')
+
+//   console.log(tableData)
+
+  var tableHead = document.createElement('thead')
+  var tableHeadTitle = document.createElement('th')
+  var tbody = document.createElement('tbody')
+  tableHeadTitle.textContent = 'Худи стандарт'
+  tableHead.append(tableHeadTitle)
+
+  var tableBody = document.createElement('tbody')
+
+//   // quantity
+
+//   var tableQuantity=document.createElement('tr')
+//   var tableQuantityTitle=document.createElement('th')
+//   tableQuantityTitle.textContent='Количество'
+//   tableQuantity.append(tableQuantityTitle)
+
+// //price
+//   var tablePrice=document.createElement('tr')
 
 
-tableQuantity.append(tCell)
-})
+//   //content
+//   tableData.Quantity?.forEach(function(item){
+//     var tableCell=document.createElement('td')
+//     tableCell.textContent=item
+//     tableQuantity.append(tableCell)
+//   })
+
+
+//   tableBody.append(tableQuantity)
+
+for(var row in tableData){
+    console.log(tableData[row])
+}
+
+
+  table.append(tableHead)
+  table.append(tableBody)
+
+
+  tableWrapper.append(table)
+}
+for(var key in tableObj){
+    tableMaker(tableObj[key])
+}
+
 
